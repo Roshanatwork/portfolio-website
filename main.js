06ce913c59dc25ee3118c347c056028fe11c26c4
Roshanatwork <99005781+Roshanatwork@users.noreply.github.com>
2025-09-07 02:18:42 +0530

    Update main.js to add project animations

--- main.js
+++ main.js
@@ -1,60 +1,63 @@
-/* MENU SHOW */ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
+/* MENU SHOW */
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show');
-        });
-    }
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
 };
-showMenu('nav-toggle','nav-menu');
+showMenu("nav-toggle", "nav-menu");
 
 /* ACTIVE AND REMOVE MENU */
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
+function linkAction() {
   /* Active link */
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
   /* Remove menu mobile */
-  const navMenu = document.getElementById('nav-menu');
-  navMenu.classList.remove('show');
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* SCROLL REVEAL ANIMATION */
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
 });
 
 /* SCROLL HOME */
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal(".home__title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
 
 /* SCROLL ABOUT */
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
 
 /* SCROLL SKILLS */
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", {});
+sr.reveal(".skills__data", { interval: 200 });
+sr.reveal(".skills__img", { delay: 600 });
+
+/* SCROLL PROJECTS */
+sr.reveal(".project__item", { interval: 200 });
 
 /* SCROLL CERTIFICATES */
-sr.reveal('.Certificates__img',{interval: 200}); 
+sr.reveal(".Certificates__img", { interval: 200 });
 
 /* SCROLL HOBBIES */
-sr.reveal('.Hobbies__img',{interval: 200}); 
+sr.reveal(".Hobbies__img", { interval: 200 });
 
 /* SCROLL CONTACT */
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal(".contact__input", { interval: 200 });
